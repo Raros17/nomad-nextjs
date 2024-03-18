ae41102c09552a0e4d3ac09db4ef7612aa2ec55d
Raros17 <sookmeoyhj@gmail.com>
2024-03-18 16:38:26 +0900

    feat: nextjs start

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,11 @@
+import Navigation from "../../components/navigation";
+
+export const metadata = {
+    title: 'Home'
+  }
+  
+export default function Page(){
+    return <div>
+        <Navigation/>
+    </div>
+}
