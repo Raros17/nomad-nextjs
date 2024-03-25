dd85ee030d985cde78cc733838debb11b475fa90
Raros17 <sookmeoyhj@gmail.com>
2024-03-25 18:07:13 +0900

    feat: PromiseAll로 상세 페이지 fetch

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,21 @@
-"use client";
 
-import { useEffect, useState } from "react";
+import Link from "next/link"
+export const metadata = {
+    title:"Home",
+}
 
+export const API_URL = "https://nomad-movies.nomadcoders.workers.dev/movies"
   
-export default function Page(){
-        const [isLoading, setIsLoading] = useState(true)
-        const [movies, setMovies] = useState();
-        const getMovies = async()=>{
-            const response = await fetch("https://nomad-movies.nomadcoders.workers.dev/movies");            
-            const json = await response.json();
-            setMovies(json);
-            setIsLoading(false)
-        }
-        useEffect(()=>{
-            getMovies()
-        },[])
+async function getMovies(){
+    //await new Promise ((resolve)=>{setTimeout(resolve, 1000)})
+    const response = await fetch(API_URL);
+    const json = await response.json();
+    return json;
+}
+
+export default async function HomePage(){
+    const movies = await getMovies();
     return <div>
-        {isLoading?"Loading":JSON.stringify(movies)}        
+        {movies.map(movie => <li key={movie.id}><Link href={`/movies/${movie.id}`}>{movie.title}</Link></li>)}
     </div>
 }
