ced42a3c2255154e7e7bad56ecbdf5751cd98192
Raros17 <sookmeoyhj@gmail.com>
2024-04-01 19:15:22 +0900

    desing: global css

--- src/components/movie-videos.tsx
+++ src/components/movie-videos.tsx
@@ -3,9 +3,8 @@ import { API_URL } from "@/app/(home)/page"
 async function getVideos(id:string){
     console.log(`Fetch video:${Date.now()}`)
     await new Promise((resolve)=>setTimeout(resolve, 3000));
-    throw new Error('something Broke...')
-    // const response = await fetch (`${API_URL}/${id}/videos`)
-    // return response.json()
+    const response = await fetch (`${API_URL}/${id}/videos`)
+    return response.json()
 }
 
 
--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -2,11 +2,11 @@
 
 import Link from "next/link"
 import { usePathname } from "next/navigation"
+import styles from "../styles/navigation.module.css"
 export default function Navigation(){
     const path= usePathname();
-    console.log(path)
     return (
-    <nav>
+    <nav className="styles.nav">
         <ul>
             <li><Link href="/">Home</Link> {path==="/"?"🥰":""}</li>
             <li><Link href="/about-us">About us</Link>{path==="/about-us"?"🥰":""}</li>
