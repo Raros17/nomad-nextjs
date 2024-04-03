658e2712844c4cddfaa46bf310091af8a1231ab9
Raros17 <sookmeoyhj@gmail.com>
2024-04-03 15:45:54 +0900

    design: home movie CSS modules

--- src/components/navigation.tsx
+++ src/components/navigation.tsx
@@ -6,7 +6,7 @@ import styles from "../styles/navigation.module.css"
 export default function Navigation(){
     const path= usePathname();
     return (
-    <nav className="styles.nav">
+    <nav className={styles.nav}>
         <ul>
             <li><Link href="/">Home</Link> {path==="/"?"🥰":""}</li>
             <li><Link href="/about-us">About us</Link>{path==="/about-us"?"🥰":""}</li>
