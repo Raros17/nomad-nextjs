63daeef9f40c36beff52427c81ce6e7f5757abcb
Raros17 <sookmeoyhj@gmail.com>
2024-04-03 16:00:42 +0900

    feat: home 레이아웃 및 useRouter 추가

--- src/components/movie.tsx
+++ src/components/movie.tsx
@@ -1,5 +1,7 @@
+"use client";
 import Link from "next/link";
 import styles from "../styles/movie.module.css"
+import { useRouter } from "next/navigation";
 
 interface MovieProps{
     title: string;
@@ -9,9 +11,14 @@ interface MovieProps{
 
 
 export default function Movie({title, id, poster_path}: MovieProps){
-    return (
+    const router = useRouter();
+
+    const onClick = () => {
+        router.push(`/movies/${id}`)
+    }
+     return (
     <div className={styles.movie}>
-        <img src={poster_path} alt={title}/> 
+        <img src={poster_path} alt={title} onClick = {onClick}/> 
         <Link href={`/movies/${id}`}>{title}</Link>
     </div>
     )
