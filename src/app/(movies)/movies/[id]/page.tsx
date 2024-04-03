e5566b2fb705c0045d38a302017b0ff26b9fb4ab
Raros17 <sookmeoyhj@gmail.com>
2024-04-03 19:05:17 +0900

    feat: generateMetadata

--- src/app/(movies)/movies/[id]/page.tsx
+++ src/app/(movies)/movies/[id]/page.tsx
@@ -1,8 +1,20 @@
 import MovieInfo from "@/components/movie-info"
 import MovieVideos from "@/components/movie-videos"
 import { Suspense } from "react"
+import { getMovie } from "@/components/movie-info"
 
-export default async function MovieDetail({params:{id},}:{params:{id:string}}){
+interface Iparams {
+    params : {id: string}
+}
+
+export async function generateMetadata({params:{id}} : Iparams){
+    const movie = await getMovie(id);
+    return {
+        title: movie.title
+    }
+}
+
+export default async function MovieDetail({params:{id}}:Iparams){
     return <div>
         <Suspense fallback={<h1>Loading Movie Info!</h1>}>
             <MovieInfo id={id}/>
