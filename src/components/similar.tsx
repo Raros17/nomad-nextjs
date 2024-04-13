3514a7aacd71465c5b42277c8913a6fd16ea5cc0
Raros17 <sookmeoyhj@gmail.com>
2024-04-13 17:37:44 +0900

    feat: similar title Link

--- src/components/similar.tsx
+++ src/components/similar.tsx
@@ -2,6 +2,7 @@
 import Image from "next/image";
 import styles from "../styles/movie-similar.module.css"
 import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 interface SimilarProps {
     id: string,
@@ -20,7 +21,9 @@ export default function Similar({title, id, poster_path, vote_average}: SimilarP
      return (
         <div key={id} className={styles.similarSection}> 
             <div className={styles.similarInfo}>
-                <h5>{title}</h5>
+                <Link href={`/movies/${id}`}>
+                    <h5>{title}</h5>
+                </Link>
                 <h6>⭐{vote_average?.toFixed(1)}</h6>
             </div>
             {poster_path?(<Image src={poster_path} alt="similar movie" width={250} height={350} className={styles.poster} onClick={() => handlePosterClick(id)} />):(
